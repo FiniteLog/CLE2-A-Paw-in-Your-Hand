26bda9f3788ba7a70118ee9f10dbdb5df26bed04
Finnw <finnwoudstra@gmail.com>
2025-01-07 13:14:15 +0100

    css and reserving

--- pages/reservering.php
+++ pages/reservering.php
@@ -1,1 +1,29 @@
+<!doctype html>
+<html lang="nl" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>reservering</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    <link rel="stylesheet" href="/CSS/style.css"
 
+</head>
+<body>
+<main class="my-6">
+    <div class="columns mx-6">
+        <button class="column is-narrow button mx-2">week 1</button>
+        <button class="column is-narrow button mx-2">week 2</button>
+        <button class="column is-narrow button mx-2">week 3</button>
+        <button class="column is-narrow button mx-2">week 4</button>
+    </div>
+</main>
+<footer class="bg-footer-top pt-5">
+    <div class="bg-footer columns">
+        <img src="/images/pupp_darkGreen.png" width="100">
+        <p class="column is-align-self-flex-end is-size-3 has-text-weight-semibold">A Paw in Your Hand</p>
+    </div>
+</footer>
+</body>
+</html>
