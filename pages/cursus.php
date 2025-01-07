10eb916df66eda64d486e4c551be435dd940d063
Nydiliria <Nydiliriayt@gmail.com>
2025-01-07 15:33:10 +0100

    updated cursus.php

--- pages/cursus.php
+++ pages/cursus.php
@@ -5,28 +5,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Bulma!</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    <link rel="stylesheet" href="/CSS/style.css">
 </head>
 <body>
-<nav class="navbar is-primary">
-    <div id="navbarBasicExample" class="navbar-menu">
+<nav class="navbar">
+    <div id="navbarBasic" class="navbar-menu my-5 mx-5">
         <div class="navbar-start">
             <a class="navbar-item">
                 Home
             </a>
-
             <a class="navbar-item">
                 Inschrijven
             </a>
-
             <a class="navbar-item">
                 Cursussen
             </a>
-
+        </div>
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <img src="/images/pupp_darkGreen.png" height="100">
+            </div>
         </div>
     </div>
 </nav>
 <header></header>
-<main></main>
-<footer></footer>
+<main class="course-mainpage">
+    <section class="course-container p-2">
+        <h1>
+            Cursus basis oefeningen
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur eveniet harum necessitatibus,
+            nesciunt nihil non numquam obcaecati quis repudiandae sequi? Animi enim error hic iure laudantium non
+            perspiciatis porro voluptatem.
+        </p>
+        <div class="image is-128x128">
+            <img src="/images/Kennemer_Hondenschool_begrijpen_van_pup_hond.jpg" height="100" alt="illustratie">
+        </div>
+
+        <a href="">Details</a>
+    </section>
+    <section class="course-container mx-2 my-2">
+        <h1>
+            Puppycursus
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt enim error hic nobis non tempore,
+            tenetur? Eius necessitatibus non officiis provident quia, rem soluta. Asperiores consectetur dignissimos
+            doloribus excepturi repellat.
+        </p>
+        <div class="image is-128x128">
+            <img src="/images/Kennemer_Hondenschool_puppycursus.jpg" height="100" alt="illustratie">
+        </div>
+
+        <a href="">Details</a>
+    </section>
+    <section class="course-container mx-2 my-2">
+        <h1>
+            Free Shaping
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dolore, eligendi fuga laboriosam mollitia
+            neque quaerat repudiandae saepe veniam voluptatibus. Atque esse est fugit itaque magnam odio quam, quis
+            sunt.
+        </p>
+        <div class="image is-128x128">
+            <img src="/images/Kennemer_Hondenschool_clickertraining.jpg" height="100px" alt="illustratie">
+        </div>
+        <a href="">Details</a>
+    </section>
+
+</main>
+<footer class=" bg-footer-top pt-5">
+    <div class="bg-footer columns">
+        <img src="/images/pupp_darkGreen.png" width="100">
+        <p class="column is-align-self-flex-end is-size-3 has-text-weight-semibold">A Paw in Your Hand</p>
+    </div>
+</footer>
 </body>
 </html>
