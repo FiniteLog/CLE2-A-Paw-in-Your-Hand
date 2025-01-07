f789cd3e2ed5db2f9be113d401393b489e5ca2c5
Finnw <finnwoudstra@gmail.com>
2025-01-07 14:14:52 +0100

    form added

--- pages/reservering.php
+++ pages/reservering.php
@@ -11,15 +11,60 @@
 
 </head>
 <body>
-<main class="my-6">
+<main class="m-6">
     <div class="columns mx-6">
+
         <button class="column is-narrow button mx-2">week 1</button>
         <button class="column is-narrow button mx-2">week 2</button>
         <button class="column is-narrow button mx-2">week 3</button>
         <button class="column is-narrow button mx-2">week 4</button>
     </div>
+    <div class="bg-footer box">
+        <div class="bg-footer-top box">
+            <div class="bg-footer box columns">
+                <p class="column box pb-6 is-radiusless ">Ma</p>
+                <p class="column box pb-6 is-radiusless ">Di</p>
+                <p class="column box pb-6 is-radiusless ">Wo</p>
+                <p class="column box pb-6 is-radiusless ">Do</p>
+                <p class="column box pb-6 is-radiusless ">Vr</p>
+                <p class="column box pb-6 is-radiusless ">Za</p>
+                <p class="column box pb-6 is-radiusless ">Zo</p>
+            </div>
+            <div class="bg-footer box mt-4 columns">
+                <div class="box column mx-2">
+                    <p>Aantal honden</p>
+                    <input class="box is-radiusless input" type="text">
+                </div>
+                <div class="box column mx-2">
+                    <p>Cursus</p>
+                    <div>
+                        <div class="select">
+                            <select>
+                                <option>Selecteer een cursus</option>
+                                <option>pipi</option>
+                                <option>poopoo</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="box column mx-2">
+                    <p>Selecteer een tijdslot</p>
+                    <p class="box is-radiusless"></p>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
-<footer class="bg-footer-top pt-5">
+<footer class=" bg-footer-top pt-5
+            ">
     <div class="bg-footer columns">
         <img src="/images/pupp_darkGreen.png" width="100">
         <p class="column is-align-self-flex-end is-size-3 has-text-weight-semibold">A Paw in Your Hand</p>
