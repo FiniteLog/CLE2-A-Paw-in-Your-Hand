b4b2ad5ef063caab79ab632a110ccbeeea50c2ac
Finnw <finnwoudstra@gmail.com>
2025-01-08 10:15:19 +0100

    finished html reserving page added margin fix for last box

--- pages/reservering.php
+++ pages/reservering.php
@@ -29,14 +29,16 @@
 
 <main class="m-6">
     <div class="bg-footer box mx-3">
-        <div class="columns mx-6">
 
+        <div class="columns mx-6">
             <button class="column is-narrow button mx-2">week 1</button>
             <button class="column is-narrow button mx-2">week 2</button>
             <button class="column is-narrow button mx-2">week 3</button>
             <button class="column is-narrow button mx-2">week 4</button>
         </div>
+
         <div class="bg-footer-top box">
+
             <div class="bg-footer box columns">
                 <p class="column box pb-6 is-radiusless ">Ma</p>
                 <p class="column box pb-6 is-radiusless ">Di</p>
@@ -44,8 +46,9 @@
                 <p class="column box pb-6 is-radiusless ">Do</p>
                 <p class="column box pb-6 is-radiusless ">Vr</p>
                 <p class="column box pb-6 is-radiusless ">Za</p>
-                <p class="column box pb-6 is-radiusless ">Zo</p>
+                <p class="column box pb-6 is-radiusless box-last">Zo</p>
             </div>
+
             <div class="bg-footer box mt-4 columns is-multiline is-centered">
                 <div class="box column is-3 mx-4">
                     <p>Aantal honden</p>
@@ -69,17 +72,19 @@
                     <p>Uw naam</p>
                     <input class="box is-radiusless input" type="text">
                 </div>
-                <div class="column box is-4 mx-4">
+                <div class="column box is-4 mx-4 box-last">
                     <p>Telefoonnummer</p>
                     <input class="box is-radiusless input" type="tel">
                 </div>
             </div>
+
             <div class="box bg-footer">
                 <div class="px-6 is-half column container">
                     <h1 class="has-text-centered is-underlined is-size-5">Vraag?</h1>
                     <input class="box is-radiusless input" type="text">
                 </div>
             </div>
+
         </div>
     </div>
 </main>
