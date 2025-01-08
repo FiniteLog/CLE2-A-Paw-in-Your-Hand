573c17a3f3082bed92fdedfb396f48bc60dd6819
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2025-01-08 12:10:18 +0100

    Update connection.php

--- pages/includes/connection.php
+++ pages/includes/connection.php
@@ -1,11 +1,11 @@
 <?php
-$host       = "localhost";
-$user       = "root";
-$password   = "";
-$database   = "cle2";
-
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "cle2";
 
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());;
+
 
