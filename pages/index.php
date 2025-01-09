6068b5fade95aea0a1035389e6f8a2606045f28e
Nydiliria <Nydiliriayt@gmail.com>
2025-01-09 15:48:49 +0100

    finished cursus.php front-end, index.php almost finished

--- pages/index.php
+++ pages/index.php
@@ -1,13 +1,57 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    <link rel="stylesheet" href="/CSS/style.css">
 </head>
-<body>
-<a href="cursus.php">Cursus.php</a>
-</body>
+<nav class="navbar">
+    <div id="navbarBasic" class="navbar-menu p-5">
+        <div class="navbar-start">
+            <a href="index.php" class="navbar-item custom-margin">
+                Home
+            </a>
+            <a href="reservering.php" class="navbar-item custom-margin">
+                Inschrijven
+            </a>
+            <a href="cursus.php" class="navbar-item custom-margin">
+                Cursussen
+            </a>
+        </div>
+        <div class="navbar-end">
+            <div class="navbar-image">
+                <img src="includes/images/pupp_darkGreen.png" height="100px">
+            </div>
+        </div>
+    </div>
+</nav>
+<main>
+    <h1> Welcome </h1>
+    <section class="box custom-box columns is-centered is-variable is-8 mx-2 my-2">
+        <div class="column is-narrow">
+            <img src="/pages/includes/images/Puppies.jpg" alt="placeholder" class="image is-128x128">
+        </div>
+        <div class="column is-flex is-flex-direction-column p-4">
+        <h2>Article Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, dolorem ipsa laborum omnis placeat quaerat tempora voluptatibus. Ad, amet assumenda deserunt doloremque excepturi exercitationem fugit pariatur porro quod saepe, sapiente.</p>
+        </div>
+    </section>
+    <section class="box custom-box columns is-centered is-variable is-8 mx-2 ">
+        <div class="column is-flex is-flex-direction-column p-4">
+            <h2>Article Title</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, dolorem ipsa laborum omnis placeat quaerat tempora voluptatibus. Ad, amet assumenda deserunt doloremque excepturi exercitationem fugit pariatur porro quod saepe, sapiente.</p>
+        </div>
+        <div class="column is-narrow">
+            <img src="/pages/includes/images/DogOnFence.jpg" alt="Placeholder" class="image is-128x128">
+        </div>
+    </section>
+</main>
+<footer class=" bg-footer-top pt-5">
+    <div class="bg-footer columns">
+        <img src="includes/images/pupp_darkGreen.png" width="100px">
+        <p class="column is-align-self-flex-end is-size-3 has-text-weight-semibold">A Paw in Your Hand</p>
+    </div>
+</footer>
 </html>
