4de6cad77ec35b10783eca8b05261cacefd26eef
Star <101126792+Starx707@users.noreply.github.com>
2025-01-09 12:00:09 +0100

    Now it works + renamed a file
    - I renamed the file cuz I made a spelling mistake, woops

--- pages/admin_cursus_overzicht.php
+++ pages/admin_cursus_overzicht.php
@@ -48,7 +48,7 @@ mysqli_close($db);
                 <th><img src="includes/images/<?= $course['image'] ?>" alt="" width="100px"></th>
                 <td><?= $course['title']?></td>
                 <td><?= $course['short_info'] ?></td>
-                <th><a href="admin_curus_aanpassen.php?course_id=<?= $course['course_id']?>">Aanpassen</a></th>
+                <th><a href="admin_cursus_aanpassen.php?course_id=<?= $course['course_id']?>">Aanpassen</a></th>
             </tr>
             </tbody>
         </table>
