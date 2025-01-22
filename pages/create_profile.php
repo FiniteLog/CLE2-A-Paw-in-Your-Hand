d1f1eb71730f38645042658852ae2ae0be962258
Finnw <finnwoudstra@gmail.com>
2025-01-22 11:30:58 +0100

    href fix

--- pages/create_profile.php
+++ pages/create_profile.php
@@ -44,6 +44,7 @@ if (isset($_POST['submit'])) {
                 $errors['phone_number'] = "Dit telefoonnummer is al in gebruik.";
             }
         }
+        header('Location: index.php');
     }
 }
 ?>
